83f48f7342b2b3962eabfb97b92351b7ee1f15d3
Emirsyah <muhammademir48@gmail.com>
2025-05-17 14:26:50 +0700

    refactor: update User entity to replace id with idUser and add taskList and categoryList relationships

--- src/main/java/prasetyo/jpa/entity/User.java
+++ src/main/java/prasetyo/jpa/entity/User.java
@@ -1,15 +1,13 @@
 package prasetyo.jpa.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -18,15 +16,15 @@ import lombok.NoArgsConstructor;
 @Table(name="users")
 public class User {
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+  private String idUser;
 
-  private String name;
+  private String username;
   private String email;
   private String password;
-  private String token;
-  
-  @ManyToOne
-  @JoinColumn(name="role_id", referencedColumnName="id")
-  private Role role;
+
+  @OneToMany(mappedBy = "user")
+  private List<RegularTask> taskList;
+
+  @OneToMany(mappedBy = "user") 
+  private List<Category> categoryList;
 }
